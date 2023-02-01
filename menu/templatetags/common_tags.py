720aac3e2c4764a93cf47242eb3628b87e478458
Erbol <esaparov03@gmail.com>
2023-02-01 10:19:30 +0600

    second commi

--- menu/templatetags/common_tags.py
+++ menu/templatetags/common_tags.py
@@ -6,11 +6,7 @@ register = template.Library()
 @register.inclusion_tag('menu/menu.html', takes_context=True)
 def show_top_menu(context):
     menu_items = Menu.objects.all()
-    category = Category.objects.all()
-    products = Products.objects.all()
     return {
         "menu_items": menu_items,
-        "category": category,
-        "products": products,
     }
 
